e398439e013c4ee82f01e3c7a4276436bae04cc5
extraoldwhiskey <mukhamedjanovjr@gmail.com>
2026-03-13 00:01:54 +0500

    localization fix

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -1,10 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './en.json';
-import uz from './uz.json';
+import ru from './ru.json';
 
 i18n.use(initReactI18next).init({
-  resources: { en: { translation: en }, uz: { translation: uz } },
+  resources: { en: { translation: en }, ru: { translation: ru } },
   lng: localStorage.getItem('lang') || 'en',
   fallbackLng: 'en',
   interpolation: { escapeValue: false },
