604803f93a07809eadf54a3d3cc0fd0aedeebe99
extraoldwhiskey <mukhamedjanovjr@gmail.com>
2026-03-12 23:01:30 +0500

    auth error fix

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -23,7 +23,7 @@ const findOrCreateUser = async (profile, provider) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
+  callbackURL: `https://carefree-vitality-production-70f0.up.railway.app/api/auth/google/callback`,
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const user = await findOrCreateUser(profile, 'google');
