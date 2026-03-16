62b4eed4c04e5fbaa4ebdaa891caa9662004847e
extraoldwhiskey <mukhamedjanovjr@gmail.com>
2026-03-16 16:19:43 +0500

    callback URL fix

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -23,7 +23,7 @@ const findOrCreateUser = async (profile, provider) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `https://backend-ei6y.onrender.com/api/auth/google/callback`,
+  callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const user = await findOrCreateUser(profile, 'google');
